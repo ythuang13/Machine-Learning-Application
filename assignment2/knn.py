4717a3d93722c897626b85459131f1a8b054c817
Yitian Huang <60416834+ythuang13@users.noreply.github.com>
2022-09-29 14:37:10 -0700

    finish 1nn implementation

--- assignment2/knn.py
+++ assignment2/knn.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Yitian Huang
+# FILENAME: knn.py
+# SPECIFICATION: Implement 1NN with LOO-CV
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 30 mins
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -24,6 +24,8 @@ with open('binary_points.csv', 'r') as csvfile:
 
 
 #loop your data to allow each instance to be your test set
+error_count = 0
+rounds = 0
 for i, instance in enumerate(db):
 
     #add the training features to the 2D array X and remove the instance that will be used for testing in this iteration.
@@ -33,9 +35,9 @@ for i, instance in enumerate(db):
     #For instance, Y = [1, 2, ,...]. Convert values to float to avoid warning messages
 
     #--> add your Python code here
-    # X =
-    # Y =
-    #testSample =
+    X = [[float(sample[0]), float(sample[1])] for i, sample in enumerate(db) if i != rounds]
+    Y = [(0 if sample[2] == "-" else 1) for i, sample in enumerate(db) if i != rounds]
+    testSample = [float(instance[0]), float(instance[1])]
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
@@ -44,12 +46,19 @@ for i, instance in enumerate(db):
     #use your test sample in this iteration to make the class prediction. For instance:
     #class_predicted = clf.predict([[1, 2]])[0]
     #--> add your Python code here
+    class_predicted = clf.predict([testSample])[0]
 
     #compare the prediction with the true label of the test instance to start calculating the error rate.
     #--> add your Python code here
+    label = 0 if instance[2] == "-" else 1
+    if label != class_predicted:
+        error_count += 1
+    
+    rounds += 1
 
 #print the error rate
 #--> add your Python code here
+print(f"{error_count=} {error_count/rounds}")
 
 
 
